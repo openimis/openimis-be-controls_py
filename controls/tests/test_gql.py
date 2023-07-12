d9b861418a1b924bf5bc81d1aed5c81636bc49cd
Christophe Philemotte <christophe.philemotte@8thcolor.com>
2023-07-12 18:09:10 +0200

    Make tests work isolated test harness and wih core test harness

--- controls/tests/test_gql.py
+++ controls/tests/test_gql.py
@@ -14,11 +14,40 @@ class ModelsTestCase(TestCase):
   DEFAULT_ADJUSTABILITY = Control.Adjustability.OPTIONAL
   DEFAULT_USAGE = 'a_form'
 
+
+  LOCAL_TEST_DATA_NAME = [DEFAULT_NAME]
+  FULL_TEST_DATA_NAME = ['Age', 'AntenatalAmountLeft', 'ApprovalOfSMS',
+    'BeneficiaryCard', 'Ceiling1', 'Ceiling2', 'CHFID', 'ClaimAdministrator',
+    'Confirmation', 'ConfirmationNo', 'ConsultationAmountLeft',
+    'ContributionCategory', 'CurrentAddress', 'CurrentDistrict',
+    'CurrentMunicipality', 'CurrentVillage', 'Ded1', 'Ded2', 'DeliveryAmountLeft',
+    'DistrictOfFSP', 'DOB', 'Education', 'ExpiryDate', 'FamilyType',
+    'FirstServicePoint', 'FSP', 'FSPCategory', 'FSPDistrict', 'Gender',
+    'GuaranteeNo', 'HFLevel', 'HospitalizationAmountLeft', 'IdentificationNumber',
+    'IdentificationType', 'InsureeEmail', 'LastName', 'lblItemCode', 'lblItemCodeL',
+    'lblItemLeftL', 'lblItemMinDate', 'lblServiceLeft', 'lblServiceMinDate',
+    'MaritalStatus', 'OtherNames', 'PermanentAddress', 'PolicyStatus', 'Poverty',
+    'ProductCode', 'Profession', 'RegionOfFSP', 'Relationship',
+    'SurgeryAmountLeft', 'TotalAdmissionsLeft', 'TotalAmount', 'TotalAntenatalLeft',
+    'TotalConsultationsLeft', 'TotalDelivieriesLeft', 'TotalSurgeriesLeft',
+    'TotalVisitsLeft', 'Vulnerability']
+
+  def generate_expected(self, names):
+    return {
+        'data': {
+          'control': {
+            'edges': [
+              {
+                'node': {
+                  'name': name
+                }
+              } for name in names
+            ]
+          }
+        }
+      }
+
   def setUp(self):
-    if "PYTEST_CURRENT_TEST" in os.environ:
-      cursor = connection.cursor()
-      cursor.execute("CREATE TABLE tblControls (FieldName nvarchar(50) NOT NULL, Adjustibility nvarchar(1) NOT NULL, Usage nvarchar(200) NULL, CONSTRAINT PK_tblControls PRIMARY KEY (FieldName ASC ))")
-      print(cursor.fetchall())
     self.query = """
     {
       control{
@@ -31,76 +60,40 @@ class ModelsTestCase(TestCase):
     }
     """
     self.control_schema = graphene.Schema(query=Query)
+    self.maxDiff = None
+    self.isolated_tests = "PYTEST_CURRENT_TEST" in os.environ
 
-  def tearDown(self):
-    if "PYTEST_CURRENT_TEST" in os.environ:
-      cursor = connection.cursor()
-      cursor.execute("DROP TABLE tblControls;")
-      print(cursor.fetchall())
-
-  def test_query_without_any_control(self):
+  def test_query_without_any_new_control(self):
     client = Client(self.control_schema)
     executed = client.execute(self.query)
-    self.assertEqual(executed, {'data': {'control': {'edges': []}}})
+    self.assertEqual(executed, self.generate_expected([] if self.isolated_tests else self.FULL_TEST_DATA_NAME))
 
   def test_query_with_one_control(self):
-    Control.objects.create(
-      name=ModelsTestCase.DEFAULT_NAME,
-      adjustability=ModelsTestCase.DEFAULT_ADJUSTABILITY,
-      usage=ModelsTestCase.DEFAULT_USAGE)
-
-    client = Client(self.control_schema)
-    executed = client.execute(self.query)
-    self.assertEqual(
-      executed,
-      {
-        'data': {
-          'control': {
-            'edges': [
-              {
-                'node': {
-                  'name': 'a_field'
-                }
-              }
-            ]
-          }
-        }
-      })
-
-  def test_query_with_several_controls(self):
-    for nbr in range(1, 4):
+    if self.isolated_tests:
       Control.objects.create(
-        name=f'{ModelsTestCase.DEFAULT_NAME}_{nbr}',
+        name=ModelsTestCase.DEFAULT_NAME,
         adjustability=ModelsTestCase.DEFAULT_ADJUSTABILITY,
         usage=ModelsTestCase.DEFAULT_USAGE)
 
     client = Client(self.control_schema)
     executed = client.execute(self.query)
     self.assertEqual(
-      executed, 
-      {
-        'data': {
-          'control': {
-            'edges': [
-              {
-                'node': {
-                  'name': 'a_field_1'
-                }
-              },
-              {
-                'node': {
-                  'name': 'a_field_2'
-                }
-              },
-              {
-                'node': {
-                  'name': 'a_field_3'
-                }
-              }
-            ]
-          }
-        }
-      })
+      executed,
+      self.generate_expected(self.LOCAL_TEST_DATA_NAME if self.isolated_tests else self.FULL_TEST_DATA_NAME))
 
+  def test_query_with_several_controls(self):
+    TEST_DATA_NAMES = []
+    if self.isolated_tests:
+      for nbr in range(1, 4):
+        name = f'{ModelsTestCase.DEFAULT_NAME}_{nbr}'
+        Control.objects.create(
+          name=name,
+          adjustability=ModelsTestCase.DEFAULT_ADJUSTABILITY,
+          usage=ModelsTestCase.DEFAULT_USAGE)
+        TEST_DATA_NAMES.append(name)
 
-
+    client = Client(self.control_schema)
+    executed = client.execute(self.query)
+    self.assertEqual(
+      executed,
+      self.generate_expected(TEST_DATA_NAMES if self.isolated_tests else self.FULL_TEST_DATA_NAME))
--- controls/tests/test_models.py
+++ controls/tests/test_models.py
@@ -1,8 +1,3 @@
-import os
-
-if "PYTEST_CURRENT_TEST" in os.environ:
-  import pytest
-
 from django.db import connection
 from django.test import TestCase
 
@@ -13,18 +8,6 @@ class ModelsTestCase(TestCase):
   DEFAULT_NAME = 'a_field'
   DEFAULT_ADJUSTABILITY = Control.Adjustability.OPTIONAL
   DEFAULT_USAGE = 'a_form'
-
-  def setUp(self):
-    if "PYTEST_CURRENT_TEST" in os.environ:
-      cursor = connection.cursor()
-      cursor.execute("CREATE TABLE tblControls (FieldName nvarchar(50) NOT NULL, Adjustibility nvarchar(1) NOT NULL, Usage nvarchar(200) NULL, CONSTRAINT PK_tblControls PRIMARY KEY (FieldName ASC ))")
-      print(cursor.fetchall())
-
-  def tearDown(self):
-    if "PYTEST_CURRENT_TEST" in os.environ:
-      cursor = connection.cursor()
-      cursor.execute("DROP TABLE tblControls;")
-      print(cursor.fetchall())
 
   def test_basic_creation(self):
     control = Control.objects.create(
