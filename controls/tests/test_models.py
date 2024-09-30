ee5afeb721239de14f9afdfefe34086140a7fdad
Kamil Malinowski <kmalinowski@soldevelo.com>
2024-09-30 17:57:01 +0200

    OM-2038 Integrated test fixes

--- controls/tests/test_models.py
+++ controls/tests/test_models.py
@@ -1,23 +1,26 @@
-from django.test import TestCase
+from django.db import connection, ProgrammingError, transaction, utils
+from django.test import TestCase, TransactionTestCase
 from django.core.exceptions import ValidationError
-
+import psycopg2
 from controls.models import Control
 
+# Create your tests here.
 
-class ModelsTestCase(TestCase):
+
+class ModelsTestCase(TestCase, TransactionTestCase):
     DEFAULT_NAME = 'a_field'
     DEFAULT_ADJUSTABILITY = Control.Adjustability.OPTIONAL
     DEFAULT_USAGE = 'a_form'
 
     def test_basic_creation(self):
         control = Control.objects.create(
-            name=self.DEFAULT_NAME,
+            name=ModelsTestCase.DEFAULT_NAME,
             adjustability=ModelsTestCase.DEFAULT_ADJUSTABILITY,
-            usage=self.DEFAULT_USAGE)
+            usage=ModelsTestCase.DEFAULT_USAGE)
 
-        self.assertEqual(control.name, self.DEFAULT_NAME)
+        self.assertEqual(control.name, 'a_field')
         self.assertEqual(control.adjustability, Control.Adjustability.OPTIONAL)
-        self.assertEqual(control.usage, self.DEFAULT_USAGE)
+        self.assertEqual(control.usage, 'a_form')
         self.assertEqual(str(control), 'Field a_field (O) for forms a_form')
 
     def test_invalid_adjustability(self):
@@ -28,12 +31,14 @@ class ModelsTestCase(TestCase):
             'F',
         ]
         for invalid_value in invalid_adjustability_values:
-            control = Control.objects.create(
-                name=f'{ModelsTestCase.DEFAULT_NAME}_{invalid_value}',
-                adjustability=invalid_value,
-                usage=ModelsTestCase.DEFAULT_USAGE)
-            with self.assertRaises(ValidationError) as context:
-                control.clean_fields()
+
+            with self.assertRaises((ValidationError, ProgrammingError, psycopg2.errors.StringDataRightTruncation, utils.DataError)) as context:
+                with transaction.atomic():
+                    control = Control.objects.create(
+                        name=f'{ModelsTestCase.DEFAULT_NAME}_{invalid_value}',
+                        adjustability=invalid_value,
+                        usage=ModelsTestCase.DEFAULT_USAGE)
+                    control.clean_fields()
 
         valid_adjustability_values = [
             'O',
